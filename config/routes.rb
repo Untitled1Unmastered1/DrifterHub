766e1713b0093fa998a368deec4a467de2583a61
gperez <git@github.com:Untitled1Unmastered1>
2020-05-11 23:15:53 -0400

    updated users controller & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
+
+  get '/signup' => 'users#new'
+
+  
   resources :comments
   resources :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#if you want to add any custom routes, add them at the top 
