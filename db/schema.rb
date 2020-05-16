15541fbc10df3660a950c940aa0dea34e43e179d
gperez <git@github.com:Untitled1Unmastered1>
2020-05-15 21:19:52 -0400

    update to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_021942) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_05_12_021338) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
