7e9deefc454e852dabf0d93586821a91c3e2829f
gperez <git@github.com:Untitled1Unmastered1>
2020-05-15 23:16:00 -0400

    sessionscontroller update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
-    
+
+    def login 
+
+    end
+
+    def create 
+        if !params[:username].empty? 
+            session[:username] = params[:username]
+            redirect_to root_path 
+        else 
+            redirect_to login_path 
+
+        end
+    end
+
+    def destroy 
+        session.delete :user_id 
+        redirect_to '/'
+    end
 end
