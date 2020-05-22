8fe6e58521d68b951e90cb4a42240543215b70c4
gperez <git@github.com:Untitled1Unmastered1>
2020-05-22 16:55:11 -0400

    class method added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ApplicationRecord
     validates :username, presence: true 
     validates :username, uniqueness: {message: "Username already in use, please try again." }
 
+    def self.deleted_user
+        Journey.all.each do |journey|
+            if journey.user == nil 
+                journey.comments.each do |c|
+                    c.destroy
+                end
+                journey.destroy 
+            end
+        end
+    end
+
+    # def self.current_user_posts
+
+    # end
+
 end
