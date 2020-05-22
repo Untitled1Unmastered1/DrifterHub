23079f59de5f42732da97ed609df69f30e2ec32f
gperez <git@github.com:Untitled1Unmastered1>
2020-05-22 18:10:18 -0400

    fixed index error with deleteduser

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -4,4 +4,15 @@ class Journey < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments 
 
+  def self.deleted_user
+    Journey.all.each do |journey|
+        if journey.user == nil 
+            journey.comments.each do |c|
+                c.destroy
+            end
+            journey.destroy 
+        end
+    end
+end
+
 end
