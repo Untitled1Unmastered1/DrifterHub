011339574706ca208b86a812aa279e43d2439894
gperez <git@github.com:Untitled1Unmastered1>
2020-06-08 13:56:18 -0400

    updated instance method for comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :user 
     belongs_to :journey
-    # before_validation :current_date
     scope :most_recent, -> {Comment.order("created_at DESC")}
 
 
@@ -12,12 +11,11 @@ class Comment < ApplicationRecord
             end
         end 
     end 
+ 
 
-    def self.current_date
-        Comment.created_at = created_at.strftime("%B %-d, %Y %-l:%M %p")
-    end #need to fix this you are so close
-
-    
+    def normalize_date
+        self.created_at = created_at.strftime("%B %-d, %Y %-l:%M %p")
+    end
 
 
 end
