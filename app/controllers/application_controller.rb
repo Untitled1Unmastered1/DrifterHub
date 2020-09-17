3152d1506deb2c36ab5d88a31d1e534600f6f222
gperez <git@github.com:Untitled1Unmastered1>
2020-09-17 17:15:43 -0400

    removed unnecessary helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,32 +2,23 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?, :validate, :owned_by_user?, :email_striper, :current_journey
 
     
-    def current_user
+    def current_user #setting the instance of @current_user to whatever user is currently in the session 
         @current_user ||= User.find_by_id(session[:user_id])
     end
 
-    def logged_in?
+    def logged_in?#in ruby there are no methods where you can ask if an object is true or false, however if
+        # we double negate something i.e. use 2 boolean operators(!!) it will esentially be the same. 
         !!current_user
     end
 
 
-    def validate 
+    def validate #validation to make sure a user can edit a journey or profile. if the user_id is not the one in the current
+        #sessions hash then they will receive a flash error and be redirected to the main page(journeys index)
         unless @user.id == current_user.id 
             flash[:error] = "Restricted area."
             redirect_to journeys_path
         end
     end 
-
-    def owned_by_user?(journeys) 
-        if @journey != nil 
-            if current_user.id == @journey.user_id
-                return true 
-            else 
-                false 
-            end
-        end
-    end
-
 
     def email_striper(email)
         username = email.split(/@gmail.com/)
