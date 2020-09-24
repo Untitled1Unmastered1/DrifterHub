394dd4be04a8eee9c0419329678fa0498d20248e
gperez <git@github.com:Untitled1Unmastered1>
2020-09-24 18:04:59 -0400

    added .uniq to index controller action

--- app/controllers/commented_journeys_controller.rb
+++ app/controllers/commented_journeys_controller.rb
@@ -2,7 +2,9 @@ class CommentedJourneysController < ApplicationController
 
     def index
         @user = User.find_by_id(params[:id])
-        @commented_journeys = @user.commented_journeys 
+        @commented_journeys = @user.commented_journeys.uniq#iterates through the array without changing it and returning
+        #unique elements, in the event that a user commented twice on the same post. to avoid having that post be displayed
+        #twice in the index view for commented_journeys 
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
 
     has_many :journeys  
     has_many :comments 
-    has_many :commented_journeys, through: :comments, source: :journey 
+    has_many :commented_journeys, through: :comments, source: :journey
 
     has_secure_password 
 
     validates :username, presence: true 
     validates :username, uniqueness: {message: "Username already in use, please try again." }
+    
 
     
 
