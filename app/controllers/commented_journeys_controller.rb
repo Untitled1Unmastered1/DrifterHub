f256637a6ee1b4b8134ae028bf7e74e5b4c38f50
gperez <git@github.com:Untitled1Unmastered1>
2020-09-24 21:54:28 -0400

    added validation to index

--- app/controllers/commented_journeys_controller.rb
+++ app/controllers/commented_journeys_controller.rb
@@ -1,7 +1,9 @@
 class CommentedJourneysController < ApplicationController
 
+
     def index
         @user = User.find_by_id(params[:id])
+        validate 
         @commented_journeys = @user.commented_journeys.uniq#iterates through the array without changing it and returning
         #unique elements, in the event that a user commented twice on the same post. to avoid having that post be displayed
         #twice in the index view for commented_journeys 
